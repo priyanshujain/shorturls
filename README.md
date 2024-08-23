3e3f2c9d2f77d58cdbce148beb7c44e21b6d8b23
PJ <ipriyanshujain@gmail.com>
2024-08-23 18:44:41 +0530

    refactor and add ip from headers

--- README.md
+++ README.md
@@ -9,16 +9,17 @@ Creates a short URL from a long URL.
 3. Create .set DATABASE_URL environment variable
 ```bash
 export DATABASE_URL="postgres://app:app@localhost:5432/app"
+export PORT=8080
 
 ```
 4. Create urls table in the database
 ```sql
-CREATE TABLE urls (
+CREATE TABLE short_links (
     short_link TEXT PRIMARY KEY,
     long_url TEXT NOT NULL,
     create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    ip TEXT NOT NULL
+    ip TEXT NOT NULL,
+    user_agent TEXT NOT NULL
 );
 ```
 5. Run `go run main.go`
-
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	log "log/slog"
 	"math/rand"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	_ "github.com/lib/pq"
@@ -39,47 +41,63 @@ func generateShortLink() string {
 	return string(b)
 }
 
-func createShortLink(db *sql.DB, longURL, ip string) (string, error) {
+type app struct {
+	db *sql.DB
+}
+
+type createShortLinkCommand struct {
+	LongURL   string
+	IP        string
+	UserAgent string
+}
+
+func (a *app) CreateShortLink(ctx context.Context, cmd createShortLinkCommand) (string, error) {
 	shortLink := generateShortLink()
-	_, err := db.Exec("INSERT INTO short_links (short_link, long_url, create_time, ip) VALUES ($1, $2, now(), $3)",
-		shortLink, longURL, ip)
+	_, err := a.db.Exec("INSERT INTO short_links (short_link, long_url, create_time, ip, user_agent) VALUES ($1, $2, now(), $3, $4)",
+		shortLink, cmd.LongURL, cmd.IP, cmd.UserAgent)
 	if err != nil {
 		return "", err
 	}
 	return shortLink, nil
 }
 
-func getLongURL(db *sql.DB, shortLink string) (string, error) {
+type getLongURLQuery struct {
+	ShortLink string
+}
+
+func (a *app) LongURL(ctx context.Context, query getLongURLQuery) (string, error) {
 	var longURL string
-	err := db.QueryRow("SELECT long_url FROM short_links WHERE short_link = $1", shortLink).Scan(&longURL)
+	err := a.db.QueryRow("SELECT long_url FROM short_links WHERE short_link = $1", query.ShortLink).Scan(&longURL)
 	if err != nil {
 		return "", err
 	}
 	return longURL, nil
 }
 
-func createLinkHandler(w http.ResponseWriter, r *http.Request) {
-	db, err := connectDB()
-	if err != nil {
-		log.Error("Failed to connect to database", "error", err)
-		http.Error(w, "Failed to connect to database", http.StatusInternalServerError)
-		return
-	}
-	defer db.Close()
+type httpHandler struct {
+	http.ServeMux
+	app *app
+}
 
-	if r.Method != "POST" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
+func (h *httpHandler) createShortLink(w http.ResponseWriter, r *http.Request) {
 	longURL := r.FormValue("long_url")
 	if longURL == "" {
 		http.Error(w, "Missing long URL", http.StatusBadRequest)
 		return
 	}
 
+	// check for X-Forwarded-For header
+	if ip := r.Header.Get("X-Forwarded-For"); ip != "" {
+		r.RemoteAddr = ip
+	}
 	ip := r.RemoteAddr
-	shortLink, err := createShortLink(db, longURL, ip)
+
+	userAgent := r.UserAgent()
+	shortLink, err := h.app.CreateShortLink(r.Context(), createShortLinkCommand{
+		LongURL:   longURL,
+		IP:        ip,
+		UserAgent: userAgent,
+	})
 	if err != nil {
 		log.Error("Failed to create short link", "error", err)
 		http.Error(w, "Failed to create short link", http.StatusInternalServerError)
@@ -90,27 +108,37 @@ func createLinkHandler(w http.ResponseWriter, r *http.Request) {
 	Response(url).Render(r.Context(), w)
 }
 
-func redirectHandler(w http.ResponseWriter, r *http.Request) {
-	db, err := connectDB()
-	if err != nil {
-		http.Error(w, "Failed to connect to database", http.StatusInternalServerError)
-		return
-	}
-	defer db.Close()
-
+func (h *httpHandler) redirect(w http.ResponseWriter, r *http.Request) {
 	shortLink := r.URL.Path[1:]
 	if shortLink == "" {
 		http.Error(w, "Invalid short link", http.StatusBadRequest)
 		return
 	}
 
-	longURL, err := getLongURL(db, shortLink)
+	longURL, err := h.app.LongURL(r.Context(), getLongURLQuery{ShortLink: shortLink})
 	if err != nil {
 		http.Error(w, "Short link not found", http.StatusNotFound)
 		return
 	}
 
 	http.Redirect(w, r, longURL, http.StatusFound)
+}
+
+func (h *httpHandler) init() {
+	h.HandleFunc("/create", h.createShortLink)
+	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			Index().Render(r.Context(), w)
+		} else {
+			h.redirect(w, r)
+		}
+	})
+}
+
+func NewHTTPHandler(app *app) http.Handler {
+	h := &httpHandler{app: app}
+	h.init()
+	return panicHandler(h)
 }
 
 func main() {
@@ -120,17 +148,24 @@ func main() {
 	}
 	defer db.Close()
 
-	http.HandleFunc("/create", createLinkHandler)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/" {
-			Index().Render(r.Context(), w)
-		} else {
-			redirectHandler(w, r)
-		}
-	})
-
-	err = http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err)
+	a := &app{db: db}
+	h := NewHTTPHandler(a)
+	port := os.Getenv("PORT")
+	port = strings.TrimSpace(port)
+	if port == "" {
+		port = "8080"
 	}
+	http.ListenAndServe(fmt.Sprintf(":%s", port), h)
+}
+
+func panicHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Error("panic while handling http request", "recover", r)
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		}()
+		h.ServeHTTP(w, r)
+	})
 }
